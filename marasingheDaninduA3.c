055f5641e0c365032e4b9e720b5913f324271e50
bmarasi <danindum@outlook.com>
2020-12-02 16:34:00 -0500

    header comments

---
+++ marasingheDaninduA3.c
@@ -0,0 +1,17 @@
+/************************marasingheDaninduA3.c**************
+Student Name: Danindu Marasinghe Email Id: dmarasin@uoguelph.ca
+Due Date: Course Name: CIS 1300
+ I have exclusive control over this submission via my password.
+ By including this statement in this header comment, I certify that:
+ 1) I have read and understood the University policy on academic
+ integrity;
+ 2) I have completed the Computing with Integrity Tutorial on
+ Moodle; and
+ 3) I have achieved at least 80% in the Computing with Integrity
+ Self Test.
+I assert that this work is my own. I have appropriately acknowledged
+any and all material that I have used, whether directly quoted or
+paraphrased. Furthermore, I certify that this assignment was prepared
+by me specifically for this course.
+ **********************************************************/
+
