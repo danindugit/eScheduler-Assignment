2aadae2ae478190c66879cba1d9102505929ac64
bmarasi <danindum@outlook.com>
2020-12-02 23:54:23 -0500

    Finished main menu for tasks 1-8

--- marasingheDaninduA3.c
+++ marasingheDaninduA3.c
@@ -17,91 +17,94 @@ by me specifically for this course.
 
 #include "givenA3.h"
 
-int main (int argc, char* argv[]){
-    int i;
-    int j;
-    int numPTeachingNC;
-    int numPTeachingNLevelC;
-    int cWithNP;
-    char filename1[50];
-    char filename2[50];
-    char courseNames[NUMBER_COURSES][50];
-    char profNames [NUMBER_PROFS][30];
-    int courseID [NUMBER_COURSES];
-    char data[NUMBER_PROFS][NUMBER_COURSES];
-    char cNameFound[50];
-    int cNumFound;
+// int main (int argc, char* argv[]){
+//     int i;
+//     int j;
+//     int numPTeachingNC;
+//     int numPTeachingNLevelC;
+//     int cWithNP;
+//     char filename1[50];
+//     char filename2[50];
+//     char courseNames[NUMBER_COURSES][50];
+//     char profNames [NUMBER_PROFS][30];
+//     int courseID [NUMBER_COURSES];
+//     char data[NUMBER_PROFS][NUMBER_COURSES];
+//     char cNameFound[50];
+//     int cNumFound;
 
-    if(argc != 3){
-        //if incorrect number of command line args
-        printf("Usage: ./a.out courses.txt data.txt");
-        return -1;
-    }
+//     if(argc != 3){
+//         //if incorrect number of command line args
+//         printf("Usage: ./a.out courses.txt data.txt");
+//         return -1;
+//     }
 
-    //get filename from 2nd command line arg and call rad CourseProfData with it 
-    strcpy(filename1, argv[1]);
-    readCourseProfData(filename1, courseNames, profNames, courseID);
+//     //get filename from 2nd command line arg and call rad CourseProfData with it 
+//     strcpy(filename1, argv[1]);
+//     readCourseProfData(filename1, courseNames, profNames, courseID);
 
-    strcpy(filename2, argv[2]);
-    readData(filename2, data);
+//     strcpy(filename2, argv[2]);
+//     readData(filename2, data);
 
-    // //test readCourseProfData
-    // for (i = 0; i < NUMBER_COURSES; i++)
-    // {
-    //     printf("%d: %s\n", courseID[i], courseNames[i]);
-    // }
-    // for (i = 0; i < NUMBER_PROFS; i++)
-    // {
-    //     printf("%s\n", profNames[i]);
-    // }
+//     // //test readCourseProfData
+//     // for (i = 0; i < NUMBER_COURSES; i++)
+//     // {
+//     //     printf("%d: %s\n", courseID[i], courseNames[i]);
+//     // }
+//     // for (i = 0; i < NUMBER_PROFS; i++)
+//     // {
+//     //     printf("%s\n", profNames[i]);
+//     // }
     
-    // //test readData
-    // for (i = 0; i < NUMBER_PROFS; i++)
-    // {
-    //     printf("{");
-    //     for (j = 0; j < NUMBER_COURSES; j++)
-    //     {
-    //         printf("%c, ", data[i][j]);
-    //     }
-    //     printf("}\n");
-    // }
+//     // //test readData
+//     // for (i = 0; i < NUMBER_PROFS; i++)
+//     // {
+//     //     printf("{");
+//     //     for (j = 0; j < NUMBER_COURSES; j++)
+//     //     {
+//     //         printf("%c, ", data[i][j]);
+//     //     }
+//     //     printf("}\n");
+//     // }
 
-    // //test numProfsTeachingNCourses
-    // printf("%d", numPTeachingNC);  
-    // numPTeachingNC = numProfsTeachingNCourses(data, 3, profNames); 
+//     // //test numProfsTeachingNCourses
+//     // printf("%d", numPTeachingNC);  
+//     // numPTeachingNC = numProfsTeachingNCourses(data, 3, profNames); 
 
-    // //test numProfsTeachingNLevelCourses
-    // numPTeachingNLevelC = numProfsTeachingNLevelCourses(data, 100, courseID, profNames);
-    // printf("%d profs teaching n level courses\n", numPTeachingNLevelC);
+//     // //test numProfsTeachingNLevelCourses
+//     // numPTeachingNLevelC = numProfsTeachingNLevelCourses(data, 100, courseID, profNames);
+//     // printf("%d profs teaching n level courses\n", numPTeachingNLevelC);
 
-    // //test coursesWithNProfs
-    // cWithNP = coursesWithNProfs(data, 3, courseNames);
-    // printf("%d courses with n profs\n", cWithNP);
+//     // //test coursesWithNProfs
+//     // cWithNP = coursesWithNProfs(data, 3, courseNames);
+//     // printf("%d courses with n profs\n", cWithNP);
 
-    // //test avgNumCourses
-    // printf("%.2f", avgNumCourses(data));
+//     // //test avgNumCourses
+//     // printf("%.2f", avgNumCourses(data));
 
-    // //test getCourseName
-    // printf("%d\n", getCourseName(1300, courseID, cNameFound, courseNames));
-    // if(getCourseName(1300, courseID, cNameFound, courseNames) == 0){
-    //     printf("course not found\n");
-    // }
-    // else{
-    //     printf("%s\n", cNameFound);
-    // }
+//     // //test getCourseName
+//     // printf("%d\n", getCourseName(1300, courseID, cNameFound, courseNames));
+//     // if(getCourseName(1300, courseID, cNameFound, courseNames) == 0){
+//     //     printf("course not found\n");
+//     // }
+//     // else{
+//     //     printf("%s\n", cNameFound);
+//     // }
 
-    //test getCourseNum
-    int found = getCourseNum("Advanced OO", courseID, &cNumFound, courseNames);
-    printf("%d\n", found);
-    if(found == 0){
-        printf("course not found\n");
-    }
-    else{
-        printf("%d\n", cNumFound);
-    }
+//     // //test getCourseNum
+//     // int found = getCourseNum("Advanced OO", courseID, &cNumFound, courseNames);
+//     // printf("%d\n", found);
+//     // if(found == 0){
+//     //     printf("course not found\n");
+//     // }
+//     // else{
+//     //     printf("%d\n", cNumFound);
+//     // }
 
-    return 0;
-}
+//     //test generateReport
+//     generateReport(data, courseID, courseNames, profNames);
+
+//     return 0;
+// }
 
 void readCourseProfData(char filename [50], char courseNames[NUMBER_COURSES][50], char profNames[NUMBER_PROFS][30], int courseID[NUMBER_COURSES]) {
     int i;
@@ -161,17 +164,12 @@ void readData(char filename[50], char data[NUMBER_PROFS][NUMBER_COURSES]){
             data[i][j] = tolower(strin[i][j]);
         }        
     }
-
-    fclose(inFile);
 }
 
 int numProfsTeachingNCourses(char data[NUMBER_PROFS][NUMBER_COURSES], int n, char profNames[NUMBER_PROFS][30]){
     int i;
     int j;
     int counterProfs, counterCourses;
-    char strProfs [100];
-
-    strcpy(strProfs, "");
     counterProfs = 0;
 
     for (i = 0; i < NUMBER_PROFS; i++)
@@ -185,13 +183,11 @@ int numProfsTeachingNCourses(char data[NUMBER_PROFS][NUMBER_COURSES], int n, cha
         }
         if(counterCourses >= n){
             counterProfs++;
-            strcat(strProfs, profNames[i]);
-            strcat(strProfs, ", ");
+            printf("Professor %s teaches %d or more courses a year\n", profNames[i]);
         }        
     }
-    //remove final comma from strProfs
-    strProfs[strlen(strProfs) - 2] = '\0'; 
-    printf("%s\n", strProfs);  
+    
+    printf("\nNumber of profs teaching more than n courses = %d\n", counterProfs);
 
     return counterProfs;
 }
@@ -200,11 +196,9 @@ int numProfsTeachingNLevelCourses(char data[NUMBER_PROFS][NUMBER_COURSES], int n
     int counter;
     int i;
     int j;
-    char strProfs [100];
     int isTeaching[NUMBER_PROFS];
     int isTeachingAny[NUMBER_PROFS];
 
-    strcpy(strProfs, "");
     counter = 0;
 
     for (i = 0; i < NUMBER_PROFS; i++)
@@ -213,7 +207,7 @@ int numProfsTeachingNLevelCourses(char data[NUMBER_PROFS][NUMBER_COURSES], int n
         {
             if(data[i][j] == 'y')
             {
-                if(courseID[j] / 1000 != (n/100)){
+                if(courseID[j] / 1000 != n){
                     isTeaching[i] = 0;
                     break;
                 }
@@ -232,15 +226,12 @@ int numProfsTeachingNLevelCourses(char data[NUMBER_PROFS][NUMBER_COURSES], int n
         
         if(isTeaching[i] == 1 && isTeachingAny[i] == 1){
             counter++;
-            strcat(strProfs, profNames[i]);
-            strcat(strProfs, ", ");
+            printf("Professor %s teaches only %d000 level courses\n", profNames[i], n);
         }    
     }
 
-    //remove final comma from strProfs
-    strProfs[strlen(strProfs) - 2] = '\0'; 
-    printf("%s\n", strProfs);  
-    
+    printf("Number of profs teaching n-level courses = %d\n", counter);
+
     return counter;
 }
 
@@ -248,11 +239,9 @@ int coursesWithNProfs(char data[NUMBER_PROFS][NUMBER_COURSES], int n, char cours
     int i;
     int j;
     int numProfs[NUMBER_COURSES];
-    char strCourses[100];
     int numCourses;
 
     numCourses = 0;
-    strcpy(strCourses, "");
 
     for (i = 0; i < NUMBER_COURSES; i++)
     {
@@ -269,14 +258,11 @@ int coursesWithNProfs(char data[NUMBER_PROFS][NUMBER_COURSES], int n, char cours
     {
         if(numProfs[i] == n){
             numCourses++;
-            strcat(strCourses, courseNames[i]);
-            strcat(strCourses, ", ");
+            printf("%s is taught by %d profs\n", courseNames[i], n);
         }
     }
 
-    //remove final comma from strCourses
-    strCourses[strlen(strCourses) - 2] = '\0'; 
-    printf("%s\n", strCourses);   
+    printf("Number of courses taught by n professors = %d\n", numCourses);
     
     return numCourses;
 }
@@ -332,4 +318,33 @@ int getCourseNum (char cName [50], int courseID[NUMBER_COURSES], int * cNumFound
     }
     //course not found
     return 0;
+}
+
+void generateReport(char data[NUMBER_PROFS][NUMBER_COURSES],int courseID[NUMBER_COURSES],char courseNames[NUMBER_COURSES][50],char profNames[NUMBER_PROFS][30]){
+    int i;
+    int j;
+    int k;
+    int numProfs[NUMBER_COURSES];
+    char names[NUMBER_PROFS*10][30];
+
+    //loop to get the the number of profs teaching each course
+    for (i = 0; i < NUMBER_COURSES; i++)
+    {
+        numProfs[i] = 0;
+        for (j = 0; j < NUMBER_PROFS; j++)
+        {
+            if(data[j][i] == 'y'){
+                numProfs[i]++;
+            }
+        }        
+    }
+    
+
+    printf("*************************\n");
+
+    for (i = 0; i < NUMBER_COURSES; i++)
+    {
+        printf("%d.\tCourse No: CIS%d\n", i+1, courseID[i]);
+        printf("\tCourse Name: %s\n", courseNames[i]);
+    }
 }
---
+++ marasingheDaninduA3Main.c
@@ -0,0 +1,133 @@
+#include "givenA3.h"
+
+int main(int argc, char* argv[]){
+    int i;
+    int j;
+    char filename1[50];
+    char filename2[50];
+    char courseNames[NUMBER_COURSES][50];
+    char profNames [NUMBER_PROFS][30];
+    int courseID [NUMBER_COURSES];
+    char data[NUMBER_PROFS][NUMBER_COURSES];
+
+    int option;
+    //variables for task 3
+    int numPTeachingNC;
+    int numCoursesTask3;
+    //variables for task 4
+    int level;
+    int numPTeachingNLevelC;
+    //variables for task 5
+    int numProfsForCourse;
+    int cWithNP;
+    //variables for task 7
+    int id;
+    char cNameFound[50];
+    int foundTask7;
+    //variables for task 8
+    char name [30];
+    int cNumFound;
+    int foundTask8;
+
+    //initialize
+    option = 0;
+
+    if(argc != 3){
+        //if incorrect number of command line args
+        printf("Usage: ./a.out courses.txt data.txt");
+        return -1;
+    }
+
+    //get filename from 2nd command line arg and call rad CourseProfData with it 
+    strcpy(filename1, argv[1]);
+    readCourseProfData(filename1, courseNames, profNames, courseID);
+
+    strcpy(filename2, argv[2]);
+    readData(filename2, data);
+
+    do{
+    printf("***************************\n");
+    printf("Enter a choice from the menu displayed below:\n");
+    printf("3 for Task 3 - count and print all profs teaching n courses\n");
+    printf("4 for Task 4 - count and print all profs teaching only n-level courses\n");
+    printf("5 for Task 5 - count and print all courses taught by n profs\n");
+    printf("6 for Task 6 - find and print the average number of courses taught by a prof this year\n");
+    printf("7 for Task 7 - search for course name, given its ID\n");
+    printf("8 for Task 8 - search for course ID, given its name\n");
+    printf("9 for Task 9 (additional functionality) - generate a report\n");
+    printf("Enter 10 to exit\n");
+
+    scanf("%d", &option);
+    getchar();
+
+    if(option == 3){
+        printf("TASK 3:\n");
+        printf("Number of courses?\n");
+        scanf("%d", &numCoursesTask3);
+            
+        numPTeachingNC = numProfsTeachingNCourses(data, numCoursesTask3, profNames);
+    }
+    else if(option == 4){
+        printf("TASK 4:\n");
+        printf("Level of Course?\n");
+        scanf("%d", &level);
+
+        numPTeachingNLevelC = numProfsTeachingNLevelCourses(data, level, courseID, profNames);
+    }
+    else if(option == 5){
+        printf("TASK 5:\n");
+        printf("Number of profs for a course?\n");
+        scanf("%d", &numProfsForCourse);
+
+        cWithNP = coursesWithNProfs(data, numProfsForCourse, courseNames);
+    }
+    else if(option == 6){
+        printf("TASK 6:\n");
+        printf("Average number of courses taught by a prof = %.2f\n", avgNumCourses(data));
+    }
+    else if(option == 7){
+        printf("TASK 7:\n");
+        printf("Search for which course id?\n");
+        scanf("%d", &id);
+
+        foundTask7 = getCourseName(id, courseID, cNameFound, courseNames);
+
+        if(foundTask7 == 1){
+            //found
+            printf("Course name for %d is %s\n", id, cNameFound);
+        }
+        else{
+            //not found
+            printf("Course name not found for %d\n", id);
+        }
+    }
+    else if(option == 8){
+        printf("TASK 8:\n");
+        printf("Search for which course name?\n");
+        fgets(name, 30, stdin);
+
+        //eliminate end-of-line char
+        if(name[strlen(name) - 1] == '\n'){
+            name[strlen(name) - 1] = '\0';
+        }
+
+        foundTask8 = getCourseNum(name, courseID, &cNumFound, courseNames);
+
+        if(foundTask8 == 1){
+            //found
+            printf("Course ID for %s is %d\n", name, cNumFound);
+        }
+        else{
+            //not found
+            printf("Course ID not found for %s\n", name);
+        }
+    }
+    else if(option == 9){
+
+    }
+    else{
+        break;
+    }
+    } while(option > 2 && option < 10);
+
+}
